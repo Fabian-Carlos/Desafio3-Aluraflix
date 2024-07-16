0f4758eff8303ceac02c7987a91df1976526bd06
Fabian-Carlos <166092850+Fabian-Carlos@users.noreply.github.com>
2024-07-16 17:08:34 -0500

    Add files via upload

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,22 @@
+import { CircleFadeLoader } from 'react-loaders-kit';
+import './Loading.css';
+
+const Loading = () => {
+
+
+    const loaderProps = {
+        loading: true,
+        size: 100,
+        duration: 1,
+    };
+
+    return (
+        <div className="loading-container">
+            <CircleFadeLoader {...loaderProps} />
+            <h2>Actualizando</h2>
+            
+        </div>
+    );
+};
+
+export default Loading;
